52a71993d12c3dc49643b28b9d6d0c7c27828691
Clover <smarig16@ru.is>
2018-10-15 21:38:42 +0000

    Created yet another componenent: ListInput

---
+++ components/ListInput.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import {
+	TouchableOpacity,
+	View,
+	Text,
+	TextInput,
+	StyleSheet
+} from 'react-native';
+import { createNewTodo } from '../actions/todoActions';
+import { connect } from 'react-redux';
+
+const styles = StyleSheet.create({
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-evenly',
+		marginBottom: 15,
+		paddingTop: 10,
+		paddingBottom: 10,
+		borderBottomWidth: 1,
+		borderStyle: 'dashed',
+		borderColor: 'gray'
+	},
+	input: {
+		flex: 5,
+		borderColor: 'gray',
+		borderWidth: 1,
+		paddingLeft: 4,
+		marginLeft: 8
+	},
+	inputBtn: {
+		flex: 1,
+		paddingTop: 15,
+		paddingBottom: 15,
+		marginRight: 4,
+		marginLeft: 4,
+		borderWidth: 1,
+		borderStyle: 'solid',
+		borderColor: 'gray',
+		backgroundColor: '#0a4784',
+		borderRadius: 10
+	},
+	inputBtnText: {
+		textAlign: 'center',
+		fontSize: 18,
+		color: 'white'
+	},
+	btn: {
+		marginBottom: 15,
+		paddingTop: 10,
+		paddingBottom: 10,
+		borderBottomWidth: 1,
+		borderStyle: 'dashed',
+		borderColor: 'gray'
+	},
+	btnText: {
+		paddingTop: 12,
+		paddingBottom: 12,
+		textAlign: 'center',
+		fontSize: 24
+	}
+});
+
+class ListInput extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			inputBox: '',
+			inputIsActive: false
+		};
+	}
+
+	toggleInput() {
+		const { inputIsActive } = this.state;
+		this.setState({
+			inputIsActive: !inputIsActive,
+			inputBox: ''
+		});
+	}
+
+	addTodoItem(item) {
+		const minKey = 1;
+		let maxKey = 0;
+		this.setState({ inputBox: '' });
+		const { createNewTodo, todos } = this.props;
+
+		if (todos.length === 0) {
+			createNewTodo(item, minKey.toString());
+			return;
+		}
+
+		todos.todo.map(todo => {
+			if (todo.key > maxKey) {
+				maxKey = todo.key;
+			}
+		});
+
+		createNewTodo(item, (Number(maxKey) + 1).toString());
+	}
+
+	render() {
+		const { inputIsActive, inputBox } = this.state;
+		if (inputIsActive) {
+			return (
+				<View style={styles.inputContainer}>
+					<TextInput
+						style={styles.input}
+						placeholder={'What has to be done...'}
+						underlineColorAndroid={'rgba(0,0,0,0)'}
+						onChangeText={text => this.setState({ inputBox: text })}
+						onBlur={() => {
+							if (inputBox === '') {
+								this.toggleInput();
+							}
+						}}
+						autoFocus={true}
+						value={inputBox}
+					/>
+					<TouchableOpacity
+						style={styles.inputBtn}
+						onPress={() => {
+							if (inputBox != '') {
+								this.addTodoItem(inputBox);
+								this.toggleInput();
+							}
+						}}
+						activeOpacity={0.5}>
+						<Text style={styles.inputBtnText}>Add</Text>
+					</TouchableOpacity>
+				</View>
+			);
+		}
+		return (
+			<TouchableOpacity
+				onPress={() => this.toggleInput()}
+				style={styles.btn}
+				activeOpacity={0.5}>
+				<Text style={styles.btnText}>Add new todo item</Text>
+			</TouchableOpacity>
+		);
+	}
+}
+
+const mapStateToProps = state => {
+	return {
+		todos: state
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	{ createNewTodo }
+)(ListInput);
--- components/TodoList.js
+++ components/TodoList.js
@@ -1,38 +1,15 @@
 import React from 'react';
-import {
-	View,
-	Text,
-	FlatList,
-	StyleSheet,
-	TouchableOpacity,
-	TextInput
-} from 'react-native';
-import Swipeable from 'react-native-swipeable';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
-import { getAllTodos, createNewTodo } from '../actions/todoActions';
+import { getAllTodos } from '../actions/todoActions';
 
 import ListItem from './ListItem';
+import ListInput from './ListInput';
 
 const styles = StyleSheet.create({
 	container: {
 		marginTop: 18,
 		backgroundColor: '#fff'
-	},
-	todoItemContainer: {
-		flex: 1,
-		flexDirection: 'row',
-		paddingTop: 10,
-		paddingBottom: 10,
-		borderBottomWidth: 1,
-		borderColor: 'gray'
-	},
-	infoContainer: {
-		paddingLeft: 8,
-		justifyContent: 'space-around',
-		flex: 1
-	},
-	text: {
-		fontSize: 24
 	},
 	listHeader: {
 		marginTop: 14,
@@ -41,170 +18,13 @@ const styles = StyleSheet.create({
 		paddingBottom: 10,
 		borderBottomWidth: 1,
 		borderColor: 'gray'
-	},
-	btn: {
-		marginBottom: 15,
-		paddingTop: 10,
-		paddingBottom: 10,
-		borderBottomWidth: 1,
-		borderStyle: 'dashed',
-		borderColor: 'gray'
-	},
-	btnText: {
-		paddingTop: 12,
-		paddingBottom: 12,
-		textAlign: 'center',
-		fontSize: 24
-	},
-	inputContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-		marginBottom: 15,
-		paddingTop: 10,
-		paddingBottom: 10,
-		borderBottomWidth: 1,
-		borderStyle: 'dashed',
-		borderColor: 'gray'
-	},
-	input: {
-		flex: 5,
-		borderColor: 'gray',
-		borderWidth: 1,
-		paddingLeft: 4,
-		marginLeft: 8
-	},
-	inputBtn: {
-		flex: 1,
-		paddingTop: 15,
-		paddingBottom: 15,
-		marginRight: 4,
-		marginLeft: 4,
-		borderWidth: 1,
-		borderStyle: 'solid',
-		borderColor: 'gray',
-		backgroundColor: '#0a4784',
-		borderRadius: 10
-	},
-	inputBtnText: {
-		textAlign: 'center',
-		fontSize: 18,
-		color: 'white'
-	},
-	swipeText: {
-		fontSize: 18,
-		color: 'white'
-	},
-	rightSwipeItem: {
-		flex: 1,
-		justifyContent: 'center',
-		paddingLeft: 8,
-		backgroundColor: '#b20e0e',
-		borderRadius: 10,
-		height: 40
 	}
 });
 
 class TodoList extends React.Component {
-	constructor(props) {
-		super(props);
-
-		this.state = {
-			inputBox: '',
-			inputIsActive: false
-		};
-	}
-
 	componentDidMount() {
 		const { getAllTodos } = this.props;
 		getAllTodos();
-	}
-
-	toggleInput() {
-		const { inputIsActive } = this.state;
-		this.setState({
-			inputIsActive: !inputIsActive,
-			inputBox: ''
-		});
-	}
-
-	addTodoItem(item) {
-		const minKey = 1;
-		let maxKey = 0;
-		this.setState({ inputBox: '' });
-		const { createNewTodo, todos } = this.props;
-
-		if (todos.length === 0) {
-			createNewTodo(item, minKey.toString());
-			return;
-		}
-
-		todos.todo.map(todo => {
-			if (todo.key > maxKey) {
-				maxKey = todo.key;
-			}
-		});
-
-		createNewTodo(item, (Number(maxKey) + 1).toString());
-	}
-
-	renderTodoItem = ({ item }) => (
-		<Swipeable
-			style={styles.todoItemContainer}
-			rightButtons={[
-				<TouchableOpacity
-					onPress={() => this.deleteToDoItem(item)}
-					style={styles.rightSwipeItem}>
-					<Text style={styles.swipeText}>Delete</Text>
-				</TouchableOpacity>
-			]}>
-			<View style={styles.infoContainer}>
-				<Text key={item.key} style={styles.text}>
-					{item.description}
-				</Text>
-			</View>
-		</Swipeable>
-	);
-
-	renderInput() {
-		const { inputIsActive, inputBox } = this.state;
-		if (inputIsActive) {
-			return (
-				<View style={styles.inputContainer}>
-					<TextInput
-						style={styles.input}
-						placeholder={'What has to be done...'}
-						underlineColorAndroid={'rgba(0,0,0,0)'}
-						onChangeText={text => this.setState({ inputBox: text })}
-						onBlur={() => {
-							if (inputBox === '') {
-								this.toggleInput();
-							}
-						}}
-						autoFocus={true}
-						value={inputBox}
-					/>
-					<TouchableOpacity
-						style={styles.inputBtn}
-						onPress={() => {
-							if (inputBox != '') {
-								this.addTodoItem(inputBox);
-								this.toggleInput();
-							}
-						}}
-						activeOpacity={0.5}>
-						<Text style={styles.inputBtnText}>Add</Text>
-					</TouchableOpacity>
-				</View>
-			);
-		}
-		return (
-			<TouchableOpacity
-				onPress={() => this.toggleInput()}
-				style={styles.btn}
-				activeOpacity={0.5}>
-				<Text style={styles.btnText}>Add new todo item</Text>
-			</TouchableOpacity>
-		);
 	}
 
 	render() {
@@ -217,7 +37,7 @@ class TodoList extends React.Component {
 					}
 					renderItem={({ item }) => <ListItem item={item} />}
 					data={todo}
-					ListFooterComponent={this.renderInput()}
+					ListFooterComponent={() => <ListInput />}
 				/>
 			</View>
 		);
@@ -225,6 +45,7 @@ class TodoList extends React.Component {
 }
 
 const mapStateToProps = state => {
+	console.log(state);
 	return {
 		todos: state
 	};
@@ -232,5 +53,5 @@ const mapStateToProps = state => {
 
 export default connect(
 	mapStateToProps,
-	{ createNewTodo, getAllTodos }
+	{ getAllTodos }
 )(TodoList);
